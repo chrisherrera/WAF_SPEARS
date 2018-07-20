057d53ed097ef237832d8f6310df68f446ee09fd
chrisherrera <chrisherrera@users.noreply.github.com>
2018-07-20 13:40:34 -0700

    Add files via upload

--- WAF_SPEARS.py
+++ WAF_SPEARS.py
@@ -15,7 +15,6 @@ except ImportError:
 def main():
 	sleep = [0.1,0.2,0.3,0.4,0.5]
 	timeout = 5
-	URL = 'http://block.chrisputer.com'
 
 	# triggers 950103 "Path Traversal Attack"
 	path_traversal = {}
@@ -43,6 +42,7 @@ def main():
 	#struts = '%{(#_="struts").(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).(#_memberAccess?(#_memberAccess=#dm):((#container=#context["com.opensymphony.xwork2.ActionContext.container"]).(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class)).(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear()).(#context.setMemberAccess(#dm)))).(#cmd="dir").(#iswin=(@java.lang.System@getProperty("os.name").toLowerCase().contains("win"))).(#cmds=(#iswin?{"cmd.exe","/c",#cmd}:{"/bin/bash","-c",#cmd})).(#p=new java.lang.ProcessBuilder(#cmds)).(#p.redirectErrorStream(true)).(#process=#p.start()).(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}'
 
 	while True:
+		URL = 'http://block.chrisputer.com'
 		payload = random.choice(payloads)
 		if payload['location'] == 'query_string':
 			URL += payload['payload']
